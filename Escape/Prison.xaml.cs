557cbfd63d48ca83b07a1c997228731458894c99
Naserte sidementi <rokluk@seznam.cz>
2019-02-12 15:53:19 +0100

    init

--- Escape/Prison.xaml.cs
+++ Escape/Prison.xaml.cs
@@ -23,17 +23,24 @@ namespace Escape {
         int lastCommandIndex = 0;
         int actual_hp = 100;
         int actual_dopamine = 100;
+        int light1_state = 1;
         bool admin = false;
 
         private Frame parentFrame;
         DispatcherTimer dopamin_timer = new DispatcherTimer();
 
         Dictionary<string, Uri> prison_bg = new Dictionary<string, Uri>();
+        Dictionary<string, Uri> light_state_img = new Dictionary<string, Uri>();
         private void initializeBG() {
             prison_bg.Add("PrisonBG1", new Uri(@"img/bg/bg_game_1.jpg", UriKind.Relative));
             prison_bg.Add("PrisonBG2", new Uri(@"img/bg/bg_game_2.jpg", UriKind.Relative));
             prison_bg.Add("PrisonBG3", new Uri(@"img/bg/bg_game_3.jpg", UriKind.Relative));
             prison_bg.Add("PrisonBG4", new Uri(@"img/bg/bg_game_4.jpg", UriKind.Relative));
+        }
+        private void initializeLights() {
+            light_state_img.Add("Lightoff", new Uri(@"img/items/lights/lightoff.png", UriKind.Relative));
+            light_state_img.Add("Lighton", new Uri(@"img/items/lights/lighton.png", UriKind.Relative));
+            light_state_img.Add("Lightuv", new Uri(@"img/items/lights/lightuv.png", UriKind.Relative));
         }
 
         List<string> consoleCommands = new List<string> { "help", "clear", "color" };
@@ -43,8 +50,10 @@ namespace Escape {
 
         public Prison() {
             InitializeComponent();
-            initializeBars();
             initializeBG();
+            initializeBars();
+            initializeLights();
+            initializeRoom1();
         }
         public Prison(Frame parentFrame) : this() {
             this.parentFrame = parentFrame;
@@ -81,6 +90,10 @@ namespace Escape {
             dopamin_counter();
         }
 
+        void initializeRoom1() {
+            light1.Source = new BitmapImage(light_state_img["Lighton"]);
+        }
+
         void informace() {
             hp.Content = actual_hp + "%";
             dopamin.Content = actual_dopamine + "%";
@@ -98,7 +111,67 @@ namespace Escape {
             gameConsoleInput.PreviewKeyDown -= new KeyEventHandler(Sipky);
         }
 
-        // CONSOLE
+        // DOPAMIN
+        void dopamin_counter() {
+            dopamin_timer.Interval = new TimeSpan(0, 0, 0, 0, 500);
+            dopamin_timer.Tick += new EventHandler(dopamin_Tick);
+            dopamin_timer.Start();
+        }
+
+        void dopamin_Tick(object sender, EventArgs e) {
+            if (actual_dopamine > 0) {
+                actual_dopamine--;
+            }
+            drugBar.Value--;
+            dopamin.Content = actual_dopamine + "%";
+        }
+
+        // Ovládání
+        private void Controls(object sender, KeyEventArgs e) {
+            // Pauza
+            if (e.Key == Key.Escape) {
+                if (Game_pause.Visibility == Visibility.Hidden) {
+                    Game_pause.Visibility = Visibility.Visible;
+                    dopamin_timer.Stop();
+                } else {
+                    Game_pause.Visibility = Visibility.Hidden;
+                    dopamin_timer.Start();
+                }
+            }
+            // Otevření konzole
+            if (e.Key == Key.OemTilde) {
+                if (gameConsole.Visibility == Visibility.Hidden) {
+                    gameConsole.Visibility = Visibility.Visible;
+                } else {
+                    gameConsole.Visibility = Visibility.Hidden;
+                }
+            }
+        }
+
+        // -------------- Místnost 1 --------------
+
+       void lightSwitch(object sender, RoutedEventArgs e) {
+            if (light1_state == 1) {
+                light1.Source = new BitmapImage(light_state_img["Lightoff"]);
+                light1_state = 0;
+            } else {
+                light1.Source = new BitmapImage(light_state_img["Lighton"]);
+                light1_state = 1;
+            }
+        }
+
+        void lightSwitchUv(object sender, RoutedEventArgs e) {
+            if (light1_state == 0 || light1_state == 1) {
+                light1.Source = new BitmapImage(light_state_img["Lightuv"]);
+                light1_state = 2;
+            } else {
+                light1.Source = new BitmapImage(light_state_img["Lightoff"]);
+                light1_state = 0;
+            }
+
+        }
+
+        // Console
         private void gameConsole_click(object sender, RoutedEventArgs e) {
             sendGameConsoleData();
         }
@@ -236,22 +309,8 @@ namespace Escape {
             gameConsoleInput.Focus();
         }
 
-        // DOPAMIN
-        void dopamin_counter() {
-            dopamin_timer.Interval = new TimeSpan(0, 0, 0, 0, 500);
-            dopamin_timer.Tick += new EventHandler(dopamin_Tick);
-            dopamin_timer.Start();
-        }
-
-        void dopamin_Tick(object sender, EventArgs e) {
-            if (actual_dopamine > 0) {
-                actual_dopamine--;
-            }
-            drugBar.Value--;
-            dopamin.Content = actual_dopamine + "%";
-        }
         public void Sipky(object sender, KeyEventArgs e) {
-            //Poslední a předchozí příkaz
+            // Poslední a předchozí příkaz
             if (e.Key == Key.Up) {
                 lastCommandUp();
             }
@@ -259,28 +318,8 @@ namespace Escape {
             if (e.Key == Key.Down) {
                 lastCommandDown();
             }
-        }
-
-        private void Controls(object sender, KeyEventArgs e) {
-            //Pauza
-            if (e.Key == Key.Escape) {
-                if (Game_pause.Visibility == Visibility.Hidden) {
-                    Game_pause.Visibility = Visibility.Visible;
-                    dopamin_timer.Stop();
-                } else {
-                    Game_pause.Visibility = Visibility.Hidden;
-                    dopamin_timer.Start();
-                }
-            }
-            //Otevření konzole
-            if (e.Key == Key.OemTilde) {
-                if (gameConsole.Visibility == Visibility.Hidden) {
-                    gameConsole.Visibility = Visibility.Visible;
-                } else {
-                    gameConsole.Visibility = Visibility.Hidden;
-                }
-            }
-            //Odeslání příkazu
+        
+            // Odeslání příkazu
             if (e.Key == Key.Enter) {
                 sendGameConsoleData();
             }
